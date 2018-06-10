653c22cae553bff643dd7e72a49747dc91d68996
izaquiel7 <lotygun7@gmail.com>
2018-06-09 21:43:44 -0300

    imprimindo x e o devidamente

--- Velha/main.lua
+++ Velha/main.lua
@@ -12,14 +12,12 @@ tabuleiro = {}
 
 function tabuleiro:iniciar()
 
-
-
 	for i=1,3 do
-		tabuleiro[i] = {}
+	tabuleiro[i] = {}
 
 		for j=1,3 do
 
-			tabuleiro[i][j] = " "
+		tabuleiro[i][j] = " "
 		end
 	end
 end
@@ -65,10 +63,10 @@ function tabuleiro:desenharVelha()
 end
 
 
-
-function but1PrintX( event )
-
-	if event.phase == "began" then
+n = 0
+function desenharBut1( event )
+	if n == 0 and tabuleiro:validarJogada(1, 1)  then
+		if event.phase == "began" then
 
 		x = display.newLine(10,60, 90, 140)
 		y = display.newLine(90,60, 10, 140)
@@ -79,171 +77,13 @@ function but1PrintX( event )
 		x:setStrokeColor(1,0,0)
 		y:setStrokeColor(1,0,0)
 
-	tabuleiro[1][1] = "x"
-
-	tabuleiro:checarVencedor("x")
-	end
-
-end
-
-function but2PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(115,60, 195, 140)
-		y = display.newLine(195,60, 115, 140)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[1][2] = "x"
+		tabuleiro[1][1] = "x"
 
 		tabuleiro:checarVencedor("x")
-	end
-
-end
-
-function but3PrintX( event )
-
-	if event.phase == "began" then
-		x = display.newLine(215,60, 295, 140)
-		y = display.newLine(295,60, 215, 140)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[1][3] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-function but4PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(10,160, 90, 240)
-		y = display.newLine(90,160, 10, 240)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[2][1] = "x"
-
-		tabuleiro:checarVencedor("x")
-
-	end
-end
-
-function but5PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(115,160, 195, 240)
-		y = display.newLine(195,160, 115, 240)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[2][2] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-function but6PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(215,160, 295, 240)
-		y = display.newLine(295,160, 215, 240)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[2][3] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-function but7PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(10,260, 90, 340)
-		y = display.newLine(90,260, 10, 340)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[3][1] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-function but8PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(115,260, 195, 340)
-		y = display.newLine(195,260, 115, 340)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[3][2] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-function but9PrintX( event )
-
-	if event.phase == "began" then
-
-		x = display.newLine(215,260, 295, 340)
-		y = display.newLine(295,260, 215, 340)
-
-		x.strokeWidth = 5
-		y.strokeWidth = 5
-
-		x:setStrokeColor(1,0,0)
-		y:setStrokeColor(1,0,0)
-
-		tabuleiro[3][3] = "x"
-
-		tabuleiro:checarVencedor("x")
-	end
-end
-
-----Funções para printar bola
-function but1PrintO( event )
-
-	if event.phase == "began" then
-
+		n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(1, 1) then
 		bola = display.newCircle(50, 100, 40)
 		bola:setFillColor(0,0,0)
 		bola.strokeWidth = 5
@@ -252,124 +92,375 @@ function but1PrintO( event )
 		tabuleiro[1][1] ="o"
 
 		tabuleiro:checarVencedor("o")
+
+		n = 0
+		end
 	end
 end
 
+function desenharBut2( event )
+	if n == 0 and tabuleiro:validarJogada(1,2) then
+		if event.phase == "began" then
 
-	---- Verfica Jogada valida
+			x = display.newLine(115,60, 195, 140)
+			y = display.newLine(195,60, 115, 140)
 
-	function tabuleiro:validarJogada()
-		valida = true
-			
-	for i=1,3 do
-		for j=1,3 do
-			if tabuleiro[i][j] ~= " " then
-				valida =false
-			end
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[1][2] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(1, 2) then
+			bola = display.newCircle(155, 100, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[1][2] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
 		end
 	end
+end
 
-	return valida
+function desenharBut3( event )
+	if n == 0 and tabuleiro:validarJogada(1,3) then
+		if event.phase == "began" then
+			x = display.newLine(215,60, 295, 140)
+			y = display.newLine(295,60, 215, 140)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[1][3] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(1, 3) then
+			bola = display.newCircle(260, 100, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[1][3] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
 	end
+end
 
-		---Verifica o vencedor
-	function tabuleiro:checarVencedor(var)
+function desenharBut4( event )
+	if n == 0 and tabuleiro:validarJogada(2, 1) then
+		if event.phase == "began" then
+
+			x = display.newLine(10,160, 90, 240)
+			y = display.newLine(90,160, 10, 240)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[2][1] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(2, 1) then
+			bola = display.newCircle(50, 200, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[2][1] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end
+end
+
+function desenharBut5( event )
+	if n == 0 and tabuleiro:validarJogada(2,2) then
+		if event.phase == "began" then
+
+			x = display.newLine(115,160, 195, 240)
+			y = display.newLine(195,160, 115, 240)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[2][2] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(2, 2) then
+			bola = display.newCircle(155, 200, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[2][2] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end
+end
+
+function desenharBut6( event )
+	if n == 0 and tabuleiro:validarJogada(2,3) then
+		if event.phase == "began" then
+
+			x = display.newLine(215,160, 295, 240)
+			y = display.newLine(295,160, 215, 240)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[2][3] = "x"
+
+			tabuleiro:checarVencedor("x")
+		
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(2, 3) then
+			bola = display.newCircle(260, 200, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[2][3] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end	
+end
+
+function desenharBut7( event )
+	if n == 0 and tabuleiro:validarJogada(3,1) then
+		if event.phase == "began" then
+
+			x = display.newLine(10,260, 90, 340)
+			y = display.newLine(90,260, 10, 340)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[3][1] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(3, 1) then
+			bola = display.newCircle(50, 300, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[3][1] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end
+end
+
+function desenharBut8( event )
+	if n == 0 and tabuleiro:validarJogada(3,2) then
+		if event.phase == "began" then
+
+			x = display.newLine(115,260, 195, 340)
+			y = display.newLine(195,260, 115, 340)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[3][2] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(3, 2) then
+			bola = display.newCircle(155, 300, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[3][2] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end	
+end
+
+function desenharBut9( event )
+	if n == 0 and tabuleiro:validarJogada(3, 3) then
+		if event.phase == "began" then
+
+			x = display.newLine(215,260, 295, 340)
+			y = display.newLine(295,260, 215, 340)
+
+			x.strokeWidth = 5
+			y.strokeWidth = 5
+
+			x:setStrokeColor(1,0,0)
+			y:setStrokeColor(1,0,0)
+
+			tabuleiro[3][3] = "x"
+
+			tabuleiro:checarVencedor("x")
+
+			n = 1
+		end
+	else
+		if event.phase == "began" and tabuleiro:validarJogada(3, 3) then
+			bola = display.newCircle(260, 300, 40)
+			bola:setFillColor(0,0,0)
+			bola.strokeWidth = 5
+			bola:setStrokeColor(0,0,1)
+
+			tabuleiro[3][3] ="o"
+
+			tabuleiro:checarVencedor("o")
+
+			n = 0
+		end
+	end	
+end
+
+
+---- Verfica Jogada valida
+
+function tabuleiro:validarJogada(a , b)
+	valida = true
+		if tabuleiro[a][b] ~= " " then
+		valida = false
+		end
+	return valida
+end
+
+---Verifica o vencedor
+function tabuleiro:checarVencedor(var)
 
 	local options = {
-		text = var .." é o vencedor!",
-		x = 155,
-		y = 360
+	text = var .." é o vencedor!",
+	x = 155,
+	y = 360
 	}
 
 
 
-		if tabuleiro[1][1] == var and  tabuleiro[1][2] == var and tabuleiro[1][3] == var then
-			
-			texto = display.newText(options)
+	if tabuleiro[1][1] == var and  tabuleiro[1][2] == var and tabuleiro[1][3] == var then
+	texto = display.newText(options)
 
-		elseif tabuleiro[2][1] == var and  tabuleiro[2][2] == var and tabuleiro[2][3] == var then
-			
-			texto = display.newText(options)
+	elseif tabuleiro[2][1] == var and  tabuleiro[2][2] == var and tabuleiro[2][3] == var then
+	texto = display.newText(options)
 
-		elseif tabuleiro[3][1] == var and  tabuleiro[3][2] == var and tabuleiro[3][3] == var then
-			
-			texto = display.newText(options)
+	elseif tabuleiro[3][1] == var and  tabuleiro[3][2] == var and tabuleiro[3][3] == var then
+	texto = display.newText(options)
 
-		---checa colunas
-		elseif tabuleiro[1][1] == var and  tabuleiro[2][1] == var and tabuleiro[3][1] == var then
-			
-			texto = display.newText(options)
+	---checa colunas
+	elseif tabuleiro[1][1] == var and  tabuleiro[2][1] == var and tabuleiro[3][1] == var then
+	texto = display.newText(options)
 
-		elseif tabuleiro[1][2] == var and  tabuleiro[2][2] == var and tabuleiro[3][2] == var then
-			
-			texto = display.newText(options)
+	elseif tabuleiro[1][2] == var and  tabuleiro[2][2] == var and tabuleiro[3][2] == var then
+	texto = display.newText(options)
 
-		elseif tabuleiro[1][3] == var and  tabuleiro[2][3] == var and tabuleiro[3][3] == var then
-			
-			texto = display.newText(options)
+	elseif tabuleiro[1][3] == var and  tabuleiro[2][3] == var and tabuleiro[3][3] == var then
+	texto = display.newText(options)
 
-		---checa diagonais
-		elseif tabuleiro[1][1] == var and  tabuleiro[2][2] == var and tabuleiro[3][3] == var then
-			
-			texto = display.newText(options)
-	
-		elseif tabuleiro[1][3] == var and  tabuleiro[2][2] == var and tabuleiro[3][1] == var then
-			
-			texto = display.newText(options)
-		--tabuleiro:novojogo()
-		end
-
+	---checa diagonais
+	elseif tabuleiro[1][1] == var and  tabuleiro[2][2] == var and tabuleiro[3][3] == var then
+	texto = display.newText(options)
+	elseif tabuleiro[1][3] == var and  tabuleiro[2][2] == var and tabuleiro[3][1] == var then
+	texto = display.newText(options)
+	--tabuleiro:novojogo()
 	end
+end
+
 nJogadas = 1
 
 function tabuleiro:iniciarJogada()
 	while nJogadas <= 9 do
-
 		if nJogadas % 2 == 0 then 
-
-			if tabuleiro:validarJogada() then
-
-				but1:addEventListener("touch", but1PrintX)
-				but2:addEventListener("touch", but2PrintX)
-				but3:addEventListener("touch", but3PrintX)
-				but4:addEventListener("touch", but4PrintX)
-				but5:addEventListener("touch", but5PrintX)
-				but6:addEventListener("touch", but6PrintX)
-				but7:addEventListener("touch", but7PrintX)
-				but8:addEventListener("touch", but8PrintX)
-				but9:addEventListener("touch", but9PrintX)
-
-				
-				nJogadas = nJogadas + 1
-			end
-
-	
-
-	
-		else
-
-			if tabuleiro:validarJogada() then
-
-			but1:addEventListener("touch", but1PrintO)
-
+			but1:addEventListener("touch", desenharBut1)
+			but2:addEventListener("touch", desenharBut2)
+			but3:addEventListener("touch", desenharBut3)
+			but4:addEventListener("touch", desenharBut4)
+			but5:addEventListener("touch", desenharBut5)
+			but6:addEventListener("touch", desenharBut6)
+			but7:addEventListener("touch", desenharBut7)
+			but8:addEventListener("touch", desenharBut8)
+			but9:addEventListener("touch", desenharBut9)
 
 			nJogadas = nJogadas + 1
 
-			end
+		else
+
+
+		nJogadas = nJogadas + 1
+
 		end
 	end
 end
 
-	tabuleiro:desenharVelha()
-	tabuleiro:iniciarJogada()
+tabuleiro:desenharVelha()
+tabuleiro:iniciarJogada()
 
 options = {
-	text = "Jogo da velha:",
-	x = 155,
-	y = 30
+text = "Jogo da velha:",
+x = 155,
+y = 30
 }
 
 texto = display.newText(options)
-
-
-
